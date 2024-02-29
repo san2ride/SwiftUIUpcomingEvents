5a66f94e29ae7d0fd088e558011630e8dd785c82
Jason <sanchezjason@me.com>
2024-02-28 20:33:24 -0700

    swiftui/upcoming_events/MH-UpcomingEventsView

---
+++ UpcomingEvents-MH/UpcomingEvents-MH/UpcomingEventsMHApp.swift
@@ -0,0 +1,17 @@
+//
+//  UpcomingEventsMHApp.swift
+//  UpcomingEvents-MH
+//
+//  Created by Cause  I'm Electric on 2/28/24.
+//
+
+import SwiftUI
+
+@main
+struct UpcomingEventsMHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            UpcomingEventsView()
+        }
+    }
+}
---
+++ UpcomingEvents-MH/UpcomingEvents-MH/UpcomingEventsView.swift
@@ -0,0 +1,18 @@
+//
+//  UpcomingEventsView.swift
+//  UpcomingEvents-MH
+//
+//  Created by Cause  I'm Electric on 2/28/24.
+//
+
+import SwiftUI
+
+struct UpcomingEventsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UpcomingEventsView()
+}
